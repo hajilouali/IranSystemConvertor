0988f1a21ae198f42586a2dd811cabd4c0361167
hajilouali <hajilouali@gmail.com>
2020-05-22 14:20:12 +0430

    Add project files.

---
+++ Model/DSKKAR.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IranSystemConvertor.Model
+{
+   public class DSKKAR
+    {
+        public string DSK_ID { get; set; }
+        public string DSK_NAME { get; set; }
+        public string DSK_FARM { get; set; }
+        public string DSK_ADRS { get; set; }
+        public int DSK_KIND { get; set; }
+        public int DSK_YY { get; set; }
+        public int DSK_MM { get; set; }
+        public string DSK_LISTNO { get; set; }
+        public string DSK_DISC { get; set; }
+        public int DSK_NUM { get; set; }
+        public int DSK_TDD { get; set; }
+        public int DSK_TROOZ { get; set; }
+        public int DSK_TMAH { get; set; }
+        public int DSK_TMAZ { get; set; }
+        public int DSK_TMASH { get; set; }
+        public int DSK_TTOTL { get; set; }
+        public int DSK_TBIME { get; set; }
+        public int DSK_TKOSO { get; set; }
+        public int DSK_BIC { get; set; }
+        public int DSK_RATE { get; set; }
+        public int DSK_PRATE { get; set; }
+        public int DSK_BIMH { get; set; }
+        public int MON_PYM { get; set; }
+    }
+}
---
+++ Model/DSKWOR.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IranSystemConvertor.Model
+{
+   public  class DSKWOR
+    {
+        public string DSW_ID { get; set; }
+        public int DSW_YY { get; set; }
+        public int DSW_MM { get; set; }
+        public string DSW_LISTNO { get; set; }
+        public string DSW_ID1 { get; set; }
+        public string DSW_FNAME { get; set; }
+        public string DSW_LNAME { get; set; }
+        public string DSW_DNAME { get; set; }
+        public string DSW_IDNO { get; set; }
+        public string DSW_IDPLC { get; set; }
+        public string DSW_IDATE { get; set; }
+        public string DSW_BDATE { get; set; }
+        public string DSW_SEX { get; set; }
+        public string DSW_NAT { get; set; }
+        public string DSW_OCP { get; set; }
+        public string DSW_SDATE { get; set; }
+        public string DSW_EDATE { get; set; }
+        public int DSW_DD { get; set; }
+        public int DSW_ROOZ { get; set; }
+        public int DSW_MAH { get; set; }
+        public int DSW_MAZ { get; set; }
+        public int DSW_MASH { get; set; }
+        public int DSW_TOTL { get; set; }
+        public int DSW_BIME { get; set; }
+        public int DSW_PRATE { get; set; }
+        public string DSW_JOB { get; set; }
+        public string PER_NATCOD { get; set; }
+    }
+}
