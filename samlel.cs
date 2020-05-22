2eeac7bb575dd1f957042bfac84b50f353b438fc
hajilouali <hajilouali@gmail.com>
2020-05-22 14:41:20 +0430

    hh

---
+++ samlel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IranSystemConvertor;
+using System.Data.OleDb;
+using System.Data;
+using System.IO;
+using IranSystemConvertor.Model;
+using System.Reflection;
+
+namespace IranSystemConvertor
+{
+   public class samlel
+    {
+        //Stream fs = File.OpenRead(@"D:\tamin\tamin\LIST\ROJAN\99\01\DSKKAR00.dbf");
+        //var ddd = new DBFReader(fs, Encoding.GetEncoding("windows-1256"));
+        //var data = ddd.ReadToObject<DSKKAR>();
+
+
+        //string output = ConvertTo.UnicodeFrom(TextEncoding.Arabic1256, "üُ—‘ں");
+        //var test = "علی حاجیلو".ToIranSystem();
+        //Console.WriteLine(output);
+    }
+}
